2de40bf1110a1e819f8f5edb36f9b7739a327367
IngLeoCastellanos <ingleonardocastellanos@gmail.com>
2025-03-13 17:09:38 -0500

    Avance

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -5,28 +5,256 @@
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero
         span 2
-      h1 Materiales para la elaboración de moldes, tipos, características
+      h1 Moldería para bolsos, billeteras y cinturones 
 
-    p Para llevar a cabo el proceso de patronaje, es fundamental contar con los siguientes materiales y elementos:
+    p La moldería es el proceso de diseño y desarrollo de patrones para la fabricación de productos, asegurando precisión en trazos y medidas. En marroquinería, la exactitud de los moldes es crucial para la fidelidad al diseño, pudiendo realizarse de manera digital o manual; en este espacio trabajaremos la moldería manual.
+
+    .row.justify-content-center(data-aos="flip-up").fondot3.px-4
+      .col-lg-12.col-12.px-lg-5.px-4
+        .row.justify-content-start.mb-4(data-aos="zoom-in-left")
+          .col-sm-12.col-lg-7.col-12
+            .d-flex.align-items-center.mt-4
+              img.w-card-icon(src="@/assets/curso/temas/Tema6/ico.svg")
+              h3.bg2.py-2.w-card-text.text-white Partes de un bolso
+        .row.justify-content-center.align-items-center.mb-5
+          .col-lg-4.mb-2.col-sm-7
+            img(src='@/assets/curso/temas/Tema2/img01.png', alt='')
+          .col-lg-8
+            p El bolso se compone de diversas piezas, visibles o internas según el diseño:
+            ul.lista-ul
+              li.mb-0
+                i.fas.fa-check-circle
+                |#[b Frente:] parte frontal, debe cortarse en la mejor sección del material.
+              li.mb-0
+                i.fas.fa-check-circle
+                |#[b Espaldar:] pieza opuesta al frente, generalmente en contacto con el cuerpo.
+              li.mb-0
+                i.fas.fa-check-circle
+                |#[b Base:] ubicada en la parte inferior, menos visible, permitiendo aprovechar zonas con imperfecciones del material.
+              li.mb-0
+                i.fas.fa-check-circle
+                |#[b Fuelle/escudo lateral:] determina el volumen del bolso; algunos diseños pueden prescindir de esta pieza.
+              li.mb-0
+                i.fas.fa-check-circle
+                |#[b Tapa:] se une al espaldar y su presencia depende del diseño.
+              li.mb-0
+                i.fas.fa-check-circle
+                |#[b Porta cierre/falso interno:] alojamiento del cierre, generalmente en la parte superior.
+              li.mb-0
+                i.fas.fa-check-circle
+                |#[b Correa/manija:] elemento de sujeción, cuya longitud y forma dependen del diseño.
+              li.mb-0
+                i.fas.fa-check-circle
+                |#[b Forro:] puede ser adherido o suelto, según el diseño.
+              li.mb-0
+                i.fas.fa-check-circle
+                |#[b Bolsillo interno:] compartimento auxiliar que mejora la organización del usuario.
+
+    .row.justify-content-start.mb-4(data-aos="zoom-in-left")
+      .col-sm-12.col-lg-7.col-12
+        .d-flex.align-items-center.mt-4
+          img.w-card-icon(src="@/assets/curso/temas/Tema6/ico.svg")
+          h3.bg2.py-2.w-card-text.text-white Medidas de aumento para moldes
+    p Para el ensamble del bolso, se aplican márgenes de aumento en los moldes según el material y el tipo de operación:
 
     .row.justify-content-center.align-items-center.mb-5(data-aos="fade-left")
-      .col-lg-3.col-8.mb-4.col-sm-6
-        img(src='@/assets/curso/temas/Tema2/img01.png', alt='')
-      .col-lg-9
-        AcordionA.mb-5(tipo="a" clase-tarjeta="tarjeta tarjeta--azul")
-          .row(titulo="Cartulina o cartón dúplex")
-            .col-md-12.mb-4.mb-md-0
-              p Este material, compuesto por láminas de papel de mayor espesor, facilita el corte y la conformación de los moldes. Se utiliza para desarrollar la moldería de acuerdo con el diseño requerido. Comercialmente, se encuentra en pliegos de 100 x 70 cm y en diversas tonalidades en el caso de la cartulina.
-          .row(titulo="Portaminas o lápiz")
-            .col-md-12.mb-4.mb-md-0
-              p Se emplea para realizar trazos precisos sobre el papel, lo que garantiza líneas exactas en la elaboración del molde. Se recomienda utilizar minas de 0.5 a 0.7 m.m para obtener una mayor precisión en los detalles.
-          .row(titulo="Borrador")
-            .col-md-12.mb-4.mb-md-0
-              p Su función principal es corregir trazos sin generar manchas en el molde. Se recomienda el uso de borradores de miga de pan o de nata, ya que permiten hacer correcciones con mayor limpieza y precisión.
-          .row(titulo="Mina plata")
-            .col-md-12.mb-4.mb-md-0
-              p Este tipo de mina es ideal para marcar sobre materiales como cuero o sintético, ya que sus trazos son fáciles de remover. Se emplea para señalar líneas de corte de patrones y delimitar el doblez de algunas piezas, facilitando así la confección.
+      .col-lg-3.col-sm-6.col-10
+        .tarjeta.fondoslyder3.p-4
+          .row.justify-content-center.align-items-center.mb-3
+            .col-sm-6.col-lg-5.col-6
+              img(src='@/assets/curso/temas/Tema2/ico1.svg')
+          .row.justify-content-center.mb-3.px-4
+            .col-lg-12.col-sm-8
+              p #[b Montaje y unión:] 10 mm (aplicable a cuero, textil y sintético).
+      
+      .col-lg-3.col-sm-6.col-10
+        .tarjeta.fondoslyder3.p-4
+          .row.justify-content-center.align-items-center.mb-3
+            .col-sm-6.col-lg-5.col-6
+              img(src='@/assets/curso/temas/Tema2/ico1.svg')
+          .row.justify-content-center.mb-3.px-4
+            .col-lg-12.col-sm-8
+              p #[b Doblado:] proceso en el que el material se pliega sobre sí mismo o sobre un refuerzo rígido para dar forma y estructura al producto.
+              ul.lista-ul
+                li.mb-0
+                  i.fas.fa-check-circle
+                  |#[b Cuero:] 8 mm sobre sí mismo, 10 mm sobre material rígido.
+                li.mb-0
+                  i.fas.fa-check-circle
+                  |#[b Textil/Sintético:] 10 mm sobre sí mismo, 12 mm sobre material rígido.
+      
+      .col-lg-3.col-sm-6.col-10
+        .tarjeta.fondoslyder3.p-4
+          .row.justify-content-center.align-items-center.mb-3
+            .col-sm-6.col-lg-5.col-6
+              img(src='@/assets/curso/temas/Tema2/ico1.svg')
+          .row.justify-content-center.mb-3.px-4
+            .col-lg-12.col-sm-8
+              p #[b Envivado:] técnica de acabado en los bordes del cuero, donde se recorta y pule para suavizar y mejorar la estética de la pieza.
+              ul.lista-ul
+                li.mb-0
+                  i.fas.fa-check-circle
+                  |#[b Cuero:] 5 mm.
+                li.mb-0
+                  i.fas.fa-check-circle
+                  |#[b Textil/Sintético:] 10 mm.    
+    
+      .col-lg-3.col-sm-6.col-10
+        .tarjeta.fondoslyder3.p-4
+          .row.justify-content-center.align-items-center.mb-3
+            .col-sm-6.col-lg-5.col-6
+              img(src='@/assets/curso/temas/Tema2/ico1.svg')
+          .row.justify-content-center.mb-3.px-4
+            .col-lg-12.col-sm-8
+              p #[b Costuras internas:] 
+              ul.lista-ul
+                li.mb-0
+                  i.fas.fa-check-circle
+                  |#[b Cuero:] 8 mm.
+                li.mb-0
+                  i.fas.fa-check-circle
+                  |#[b Textil/Sintético:] 10 mm.   
+    
+    .row.justify-content-center(data-aos="flip-up").fondot3.px-4
+      .col-lg-12.col-12.px-lg-5.px-4
+        .row.justify-content-start.mb-4(data-aos="zoom-in-left")
+          .col-sm-12.col-lg-7.col-12
+            .d-flex.align-items-center.mt-4
+              img.w-card-icon(src="@/assets/curso/temas/Tema6/ico.svg")
+              h3.bg2.py-2.w-card-text.text-white Marcado e industrialización de moldes
+        
+        .row.justify-content-center.align-items-center.mb-5
+          .col-lg-4.mb-2.col-sm-7
+            img(src='@/assets/curso/temas/Tema2/img02.png', alt='')
+          .col-lg-8
+            p El proceso de industrialización requiere el marcado preciso de cada molde para su identificación eficiente, por ello cada patrón debe incluir:
+            ul.lista-ul
+              li.mb-0
+                i.fas.fa-check-circle
+                |#[b Nombre del molde:] correspondiente a la pieza del bolso.
 
+              li.mb-0
+                i.fas.fa-check-circle
+                |#[b Cantidad de piezas a cortar:] indicando la cantidad necesaria (ej. 2x1).
+
+              li.mb-0
+                i.fas.fa-check-circle
+                |#[b Material:] especificación del material de corte (ej. cuero, sintético, textil).
+
+              li.mb-0
+                i.fas.fa-check-circle
+                |#[b Referencia del producto:] código o nomenclatura (ej. BTP-001).
+
+              li.mb-0
+                i.fas.fa-check-circle
+                |#[b Numeración del molde:] identificación secuencial (ej. 1/6, 2/6, etc.).
+
+            .row.justify-content-center.align-items-center.mb-5(data-aos="flip-up")
+                .col-lg-12.tarjeta.color
+                  .row.align-items-center
+                    .col-lg-2.d-none.d-lg-block
+                      img.imgtamano(src='@/assets/curso/temas/Tema4/img002.svg', alt='')
+                    .col-lg-10
+                      br
+                      h5.mb-0 Nota. 
+                      p La metodología de industrialización puede variar según los procesos internos de cada empresa.
+           
+    
+    .row.justify-content-center(data-aos="flip-up")
+      .col-lg-12.col-12.px-lg-5.px-4
+        .row.justify-content-start.mb-4(data-aos="zoom-in-left")
+          .col-sm-12.col-lg-7.col-12
+            .d-flex.align-items-center.mt-4
+              img.w-card-icon(src="@/assets/curso/temas/Tema6/ico.svg")
+              h3.bg2.py-2.w-card-text.text-white Marcado e industrialización de moldes
+    
+    
+        p Existen diferentes estructuras de bolsos, una de ellas es el bolso tipo pinza, caracterizado por pinzas en la parte inferior que, al cerrarse, generan volumen o amplitud en la pieza.
+    
+    .row.justify-content-center.align-items-center.p-2.mb-5(data-aos="flip-up")
+      .col-lg-6
+        .titulo-sexto.color-acento-contenido.text-negro
+          h5.text-negro Figura 8. 
+          | Bolso tipo pinza
+        figure
+          img(src='@/assets/curso/temas/Tema2/figura8.svg', alt='La imagen presenta un bolso elegante de tamaño mediano con asas cortas y una correa larga ajustable para llevarlo al hombro. Su diseño incluye un patrón mono gramado en tonos marrón y detalles en cuero color camel. Además, cuenta con un adorno metálico colgante con el logo de la marca y herrajes dorados que complementan su estilo sofisticado. ')
+          .py-2
+          figcaption.text-negro Fuente SENA.  
+    
+    
+    p A continuación, se describen los pasos en la elaboración de los moldes:
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    .row.justify-content-center.mb-5(data-aos="zoom-in-left")
+        .col-lg-8
+          .row.justify-content-start.mb-4(data-aos="zoom-in-left")
+            .col-sm-12.col-lg-5
+              .d-flex.align-items-center.mt-3
+                h3.bg5.py-2.w-card-text Paso 1. Molde
+          p Teniendo en cuenta la dimensión del herraje se multiplica por 2 y se suma 2 cm, para el caso de una manija de mano y con un herraje de 1 pulgada, se maneja la siguiente medida: 
 </template>
 
 <script>
